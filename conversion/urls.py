7358dcab9b5882432e41655cd5af0592e8257157
nidhinkandoth <nidhinkandoth2016@gmail.com>
2016-12-27 15:28:47 +0530

    added ajax

--- conversion/urls.py
+++ conversion/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     url(r'submission/$', views.conversion_api, name= 'conversion_api'),
     url(r'conversion$', views.conversion_page, name = 'conversion_page'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
--- currency_conversion/urls.py
+++ currency_conversion/urls.py
@@ -18,10 +18,13 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     url(r'^register/$', views.register, name='register'),
     url(r'^login/$', auth_views.login,{'template_name': 'registration/login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout,{'template_name': 'registration/logo    ut.html'}, name='logout'),
     url(r'^admin/', admin.site.urls),
     url(r'^conversion/', include('conversion.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
